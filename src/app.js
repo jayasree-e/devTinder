513a6693431e39b3749f6119709ddf7a85a4fb66
jayasree-e <jayas8sree@gmail.com>
2026-02-25 17:58:23 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
  const app =express();
- app.use("/test", (req,res)=>{
-    res.send("this is test page")
+ app.get("/user", (req,res)=>{
+    res.send("this is get")
  })
- app.use("/hello",(req,res)=>{
-    res.send("This is hello");
+ app.post("/user",(req,res)=>{
+    res.send("this post");
+
  })
- app.use("/", (req,res)=>{
-    res.send("this is home");
+ app.delete("/user",(req,res)=>{
+    res.send("this is delete")
  })
+ 
  
  
  
