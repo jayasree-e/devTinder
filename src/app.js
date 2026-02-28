b453cccf2c2dd8f65f8649294f17414326d5fa19
jayasree-e <jayas8sree@gmail.com>
2026-02-28 11:04:26 +0530

    playing with multiple routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
 const express = require("express");
  const app =express();
- app.get("/user/a*c/:name/", (req,res)=>{
-    console.log(req.params)
-    console.log(req.query)
-    res.send("this is get")
- })
- app.post("/user",(req,res)=>{
-    res.send("this post");
+//  app.get("/user/a*c/:name/", (req,res)=>{
+//     console.log(req.params)
+//     console.log(req.query)
+//     res.send("this is get")
+//  })
+//  app.post("/user",(req,res)=>{
+//     res.send("this post");
 
- })
- app.delete("/user",(req,res)=>{
-    res.send("this is delete")
- })
- 
+//  })
+//  app.delete("/user",(req,res)=>{
+//     res.send("this is delete")
+//  })
+ app.use("/user",
+   (req,res,next)=>{
+   console.log("response 1");
+   next();
+   // res.send("response 1")
+ },
+ [(req,res,next)=>{
+   console.log("response 2");
+  // res.send("response 2")
+   next();
+ },
+ (req,res,next)=>{
+   console.log("response 3");
+   res.send("response 3")
+ }],
+ (req,res)=>{
+   console.log("response 4");
+   res.send("response 4")
+ }
+)
  
  
  
