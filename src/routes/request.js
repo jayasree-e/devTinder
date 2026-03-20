8b433b9fc44004e02c55574dc9c431f43a160fa7
jayasree-e <jayas8sree@gmail.com>
2026-03-20 11:53:51 +0530

    request review router

--- src/routes/request.js
+++ src/routes/request.js
@@ -43,4 +43,29 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
    }
    
 })
+requestRouter.post("/request/review/:status/:requestId", userAuth, async(req,res)=>{
+   try{
+      const loggedInUser = req.user;
+      const {status,requestId} = req.params;
+      const allowedStatus = ["accepted","rejected"];
+      if(!allowedStatus.includes(status)){
+         return res.status(400).send("Status not allowed")
+      }
+      console.log(requestId, loggedInUser._id, status)
+      const connectionRequest = await ConnectionRequest.findOne({
+         _id: requestId,
+         toUserId: loggedInUser._id,
+         status: "interested" 
+      })
+      console.log(connectionRequest)
+      if(!connectionRequest){
+         return res.status(400).json({message:"Connection request not found"})
+      }
+      connectionRequest.status= status;
+      const data = await connectionRequest.save();
+      res.json({message:"Connection request "+ status, data})
+   }catch(err){
+      res.status(400).send("Error"+err.message)
+   }
+})
 module.exports = {requestRouter}
